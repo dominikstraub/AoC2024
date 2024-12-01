dde8f278ef3f85b040f28a9a4c80b44448eb2ee6
Dominik Straub <dominik.straub@gmail.com>
2024-12-01 11:59:32 +0100

    Day01, Part2

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -13,21 +13,22 @@ struct Day01: AdventDay {
 
     // Replace this with your solution for the first part of the day's challenge.
     func part1() -> Int {
-        // Calculate the sum of the first set of input data
         let firstList = (entities.compactMap { $0.first }).sorted(by: <)
-      let secondList = (entities.compactMap { $0[1] }).sorted(by: <)
+        let secondList = (entities.compactMap { $0[1] }).sorted(by: <)
         var distance = 0
         for i in 0 ..< firstList.count {
-            print(firstList[i], secondList[i], abs(firstList[i] - secondList[i]))
             distance += abs(firstList[i] - secondList[i])
         }
         return distance
     }
 
     // Replace this with your solution for the second part of the day's challenge.
-    func part2() -> Any {
-        // Sum the maximum entries in each set of data
-        // entities.map { $0.max() ?? 0 }.reduce(0, +)
-        return 0
+    func part2() -> Int {
+        let firstList = (entities.compactMap { $0.first })
+        let secondList = (entities.compactMap { $0[1] })
+        return firstList.reduce(0) { currentFirst, number in
+            let count = secondList.reduce(0) { current, otherNumber in number == otherNumber ? current + 1 : current }
+            return currentFirst + number * count
+        }
     }
 }
