6b5ab31be2c2552a0c80160de942236d569b8f24
Dominik Straub <dominik.straub@gmail.com>
2024-12-02 13:14:43 +0100

    Part2

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -13,79 +13,58 @@ struct Day02: AdventDay {
 
     // Replace this with your solution for the first part of the day's challenge.
     func part1() -> Int {
-        var safe = 0
-        reports: for report in entities {
-            var decreased = false
-            var increased = false
-            for index in 1 ..< report.count {
-                if report[index - 1] < report[index] {
-                    increased = true
-                } else if report[index - 1] > report[index] {
-                    decreased = true
-                }
-                let diff = abs(report[index - 1] - report[index])
-                if diff < 1 || diff > 3 {
-                    continue reports
-                }
-            }
-            if decreased && increased {
-                continue
-            }
-            safe += 1
-        }
-        return safe
+        return entities.compactMap { validateReport($0, true) }.count { $0 == true }
     }
 
     // Replace this with your solution for the second part of the day's challenge.
     func part2() -> Int {
-        var safe = 0
-        reports: for report in entities {
-            var levelRemoved = false
-            var removedIndex: Int?
-            var decreased = false
-            var increased = false
-            for index in 1 ..< report.count {
-                let indexOffset = removedIndex == index - 1 ? -1 : 0
-                if report[index - 1 + indexOffset] < report[index] {
-                    increased = true
-                    if decreased {
-                        if levelRemoved {
-                            continue reports
-                        } else {
-                            levelRemoved = true
-                            removedIndex = index
-                            continue
-                        }
-                    }
-                } else if report[index - 1 + indexOffset] > report[index] {
-                    decreased = true
-                    if increased {
-                        if levelRemoved {
-                            continue reports
-                        } else {
-                            levelRemoved = true
-                            removedIndex = index
-                            continue
-                        }
-                    }
-                }
-                let diff = abs(report[index - 1 + indexOffset] - report[index])
-                if diff < 1 || diff > 3 {
+        return entities.compactMap { validateReport($0) }.count { $0 == true }
+    }
+
+    func validateReport(_ report: [Int], _ levelRemoved: Bool = false) -> Bool {
+        var decreased = false
+        var increased = false
+        for index in 1 ..< report.count {
+            if report[index - 1] < report[index] {
+                if decreased {
                     if levelRemoved {
-                        continue reports
+                        return false
                     } else {
-                        levelRemoved = true
-                        removedIndex = index
-                        continue
+                        return validateReport(Array(report.prefix(index - 1) + report.suffix(from: index)), true) ||
+                            validateReport(Array(report.prefix(index) + report.suffix(from: index + 1)), true) ||
+                            (index > 1 &&
+                                validateReport(Array(report.prefix(index - 2) + report.suffix(from: index - 1)), true))
                     }
+                } else {
+                    increased = true
+                }
+            } else if report[index - 1] > report[index] {
+                if increased {
+                    if levelRemoved {
+                        return false
+                    } else {
+                        return validateReport(Array(report.prefix(index - 1) + report.suffix(from: index)), true) ||
+                            validateReport(Array(report.prefix(index) + report.suffix(from: index + 1)), true) ||
+                            (index > 1 &&
+                                validateReport(Array(report.prefix(index - 2) + report.suffix(from: index - 1)), true))
+                    }
+                } else {
+                    decreased = true
                 }
             }
-            if decreased && increased {
-                continue
+            let diff = abs(report[index - 1] - report[index])
+            if diff < 1 || diff > 3 {
+                if levelRemoved {
+                    return false
+                } else {
+                    return validateReport(Array(report.prefix(index - 1) + report.suffix(from: index)), true) ||
+                        validateReport(Array(report.prefix(index) + report.suffix(from: index + 1)), true) ||
+                        (index > 1 &&
+                            validateReport(Array(report.prefix(index - 2) + report.suffix(from: index - 1)), true))
+                }
             }
-            safe += 1
-            print(report)
         }
-        return safe
+
+        return true
     }
 }
