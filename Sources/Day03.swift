f62a14d90927a1b8c0e493d263adc42ad23311ae
Dominik Straub <dominik.straub@gmail.com>
2024-12-03 20:32:55 +0100

    Day03, Part2

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -1,10 +1,17 @@
 import Algorithms
 import Foundation
 
+enum Instruction {
+    case mul(Int, Int)
+    case `do`
+    case dont
+    case error
+}
+
 struct Day03: AdventDay {
     var data: String
 
-    var entities: [(Int, Int)] {
+    var entities1: [(Int, Int)] {
         let pattern = "mul\\(([0-9]+),([0-9]+)\\)"
         let regex = try? NSRegularExpression(pattern: pattern)
         guard let matches = regex?.matches(in: data, options: [], range: NSRange(location: 0, length: data.utf8.count)) else { return [] }
@@ -13,11 +20,50 @@ struct Day03: AdventDay {
         }
     }
 
+    var entities2: [Instruction] {
+        let pattern = "(mul\\(([0-9]+),([0-9]+)\\))|(do\\(\\))|(don't\\(\\))"
+        let regex = try? NSRegularExpression(pattern: pattern)
+        guard let matches = regex?.matches(in: data, options: [], range: NSRange(location: 0, length: data.utf8.count)) else { return [] }
+        return matches.compactMap { match in
+            for index in 1 ..< match.numberOfRanges {
+                if let _ = Range(match.range(at: index), in: data) {
+                    switch index {
+                    case 1:
+                        return .mul(Range(match.range(at: 2), in: data).map { Int(data[$0]) }!!, Range(match.range(at: 3), in: data).map { Int(data[$0]) }!!)
+                    case 4:
+                        return .do
+                    case 5:
+                        return .dont
+                    default:
+                        return .error
+                    }
+                }
+            }
+            return .error
+        }
+    }
+
     func part1() -> Int {
-        return (entities.reduce(0) { $0 + $1.0 * $1.1 })
+        return (entities1.reduce(0) { $0 + $1.0 * $1.1 })
     }
 
     func part2() -> Int {
-        return 0
+        var mulEnabled = true
+        var value = 0
+        for instruction in entities2 {
+            switch instruction {
+            case let .mul(op1, op2):
+                if mulEnabled {
+                    value += op1 * op2
+                }
+            case .do:
+                mulEnabled = true
+            case .dont:
+                mulEnabled = false
+            case .error:
+                break
+            }
+        }
+        return value
     }
 }
