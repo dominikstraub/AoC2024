e3168036ba02f7021c157a18ab7c5c12a29ac45d
Dominik Straub <dominik.straub@gmail.com>
2024-12-03 19:47:07 +0100

    Day03, Part1

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -1,16 +1,20 @@
 import Algorithms
+import Foundation
 
 struct Day03: AdventDay {
     var data: String
 
-    var entities: [[Int]] {
-        data.split(separator: "\n").map {
-            $0.split(separator: " ").compactMap { Int($0) }
+    var entities: [(Int, Int)] {
+        let pattern = "mul\\(([0-9]+),([0-9]+)\\)"
+        let regex = try? NSRegularExpression(pattern: pattern)
+        guard let matches = regex?.matches(in: data, options: [], range: NSRange(location: 0, length: data.utf8.count)) else { return [] }
+        return matches.compactMap { match in
+            (Range(match.range(at: 1), in: data).map { Int(data[$0]) }, Range(match.range(at: 2), in: data).map { Int(data[$0]) }) as? (Int, Int)
         }
     }
 
     func part1() -> Int {
-        return 0
+        return (entities.reduce(0) { $0 + $1.0 * $1.1 })
     }
 
     func part2() -> Int {
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -4,18 +4,13 @@ import Testing
 
 struct Day03Tests {
     let testData = """
-    7 6 4 2 1
-    1 2 7 8 9
-    9 7 6 2 1
-    1 3 2 4 5
-    8 6 4 4 1
-    1 3 6 7 9
+    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 
     """
 
     @Test func testPart1() async throws {
         let challenge = Day03(data: testData)
-        #expect(String(describing: challenge.part1()) == "0")
+        #expect(String(describing: challenge.part1()) == "161")
     }
 
     @Test func testPart2() async throws {
